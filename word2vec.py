eaeba884b7247053bdad4d5455b1147bcf65d328
hevia <anthony@hevia.dev>
2024-11-12 23:56:26 -0800

    finish keyword method and start improving word2vec

--- word2vec.py
+++ word2vec.py
@@ -1,17 +1,17 @@
 from gensim.models import Word2Vec
 from datasets import load_dataset
+import json
 
 #load dataset
 dataset = load_dataset("nedjmaou/MLMA_hate_speech")
+
+with open('./data/hate_speech.json', 'r', encoding="utf-8") as json_file:
+    hate_speech = json.load(json_file)
 
 # put data in list format
 tweets = dataset['train']['tweet']
 
 #train and save model
-model = Word2Vec(tweets)
+model = Word2Vec(sentences=tweets, vector_size=100, window=5, min_count=1, workers=4)
 model.save("W2V_hate_speech.model")
-
-#get most similar words? not sure of the usage for this
-top10_most_similar = model.similar_by_word()
-top20_most_similar = model.similar_by_word(20)
-top_20_most_similar_to_hello = model.similar_by_word('hello', 20)
+sims = model.wv.most_similar('computer', topn=10) 
